778d0d4a6e769458ea2e8c8ff6cdba904a444819
Krish Prasad <krishkp00@gmail.com>
2025-05-01 20:08:47 -0500

    upload file lambda

---
+++ backend/lambdas/upload_resume_guest/lambda_function.py
@@ -0,0 +1,32 @@
+import json
+import boto3
+import base64
+import uuid
+import os
+
+s3 = boto3.client('s3')
+BUCKET_NAME = os.environ.get('BUCKET_NAME', 'resume-tailor-bucket.kp')
+
+def lambda_handler(event, context):
+    try:
+        body = json.loads(event['body'])
+        file_data = base64.b64decode(body['file'])
+        file_name = body.get('filename', f"{uuid.uuid4()}.pdf")
+        s3_key = f"guest/{file_name}"
+
+        s3.put_object(Bucket=BUCKET_NAME, Key=s3_key, Body=file_data)
+
+        return {
+            'statusCode': 200,
+            'headers': {'Content-Type': 'application/json'},
+            'body': json.dumps({
+                'message': 'Upload successful!',
+                's3_key': s3_key
+            })
+        }
+
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
