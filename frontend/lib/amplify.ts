a80eab560154a9ba6be8a30f0c9a80d4a1232525
Krish Prasad <krishkp00@gmail.com>
2025-05-08 17:33:21 -0500

    get rid of oauth in amplify

--- frontend/lib/amplify.ts
+++ frontend/lib/amplify.ts
@@ -5,27 +5,6 @@ export const amplifyConfig = {
     Cognito: {
       userPoolId: "us-east-2_c7gmXmRmx",
       userPoolClientId: "3cjdn2n907cjjsq6d71djfd28l",
-      loginWith: {
-        oauth: {
-          domain: "us-east-2c7gmxmrmx.auth.us-east-2.amazoncognito.com",
-          scopes: [
-            "email",
-            "openid",
-            "profile",
-            "phone",
-            "aws.cognito.signin.user.admin",
-          ],
-          redirectSignIn: [
-            "http://localhost:3000/callback",
-            "https://resume-tailor-kp.vercel.app/callback",
-          ],
-          redirectSignOut: [
-            "http://localhost:3000",
-            "https://resume-tailor-kp.vercel.app",
-          ],
-          responseType: "code" as const,
-        },
-      },
     },
   },
 };
